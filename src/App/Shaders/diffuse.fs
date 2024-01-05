7d90c01b26f85dfe2b6b1be960149e612a0e05ab
ksgamora <kostya.pyatigorsk@gmail.com>
2024-01-05 20:11:58 +0300

    omit redundant lines

--- src/App/Shaders/diffuse.fs
+++ src/App/Shaders/diffuse.fs
@@ -22,22 +22,13 @@ vec3 apply_M(vec3 in_vec) {
     return vec3(out4.xyz);
 }
 
-vec3 apply_V(vec3 in_vec) {
-    vec4 out4 = v * vec4(in_vec, 1.0);
-    return vec3(out4.xyz);
-}
-
-vec3 apply_MV(vec3 in_vec) {
-    vec4 out4 = v * m * vec4(in_vec, 1.0);
-    return vec3(out4.xyz);
-}
-
 void main() {
     // those are weights of different light sources
     float uncond_light_coef = 0.1, sun_light_coef = 0.6, spotlight_coef = 0.3;
 
-    // calculate sun light
     vec3 normal = normalize(Normal);
+
+    // calculate sun light
     vec3 sunPosition = apply_M(sun_position);
     vec3 sun_direction = normalize(sunPosition - position);
     float sun_lum = max(dot(sun_direction, normal), 0.0);
@@ -54,10 +45,4 @@ void main() {
 
     // calculate the result
     out_col = texture(tex_2d, vert_tex) * vec4(sun_light + spotlight + uncond_light, 1.0);
-
-    // for debugging purposes
-    // out_col = vec4(uncond_light_coef + sun_light, 1.0);
-    // out_col = vec4((spotlight_cos * 0.5) + 0.5, uncond_light_coef, uncond_light_coef, 1.0);
-    // out_col = vec4(spotlight_lum, uncond_light_coef, uncond_light_coef, 1.0);
-    // out_col = vec4(normal.xyz, 1.0);
 }
