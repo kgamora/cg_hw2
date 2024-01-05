2025582033b63768911f0d414bed37b150ec9384
ksgamora <kostya.pyatigorsk@gmail.com>
2024-01-05 20:18:52 +0300

    omit redundant function

--- src/App/Shaders/diffuse.fs
+++ src/App/Shaders/diffuse.fs
@@ -17,19 +17,14 @@ in vec2 vert_tex;
 
 out vec4 out_col;
 
-vec3 apply_M(vec3 in_vec) {
-    vec4 out4 = m * vec4(in_vec, 1.0);
-    return vec3(out4.xyz);
-}
-
 void main() {
     // those are weights of different light sources
-    float uncond_light_coef = 0.1, sun_light_coef = 0.6, spotlight_coef = 0.3;
+    float ambient_light_coef = 0.1, sun_light_coef = 0.6, spotlight_coef = 0.3;
 
     vec3 normal = normalize(Normal);
 
     // calculate sun light
-    vec3 sunPosition = apply_M(sun_position);
+    vec3 sunPosition = vec3(m * vec4(sun_position, 1.0));
     vec3 sun_direction = normalize(sunPosition - position);
     float sun_lum = max(dot(sun_direction, normal), 0.0);
     vec3 sun_light = (sun_light_coef * sun_lum) * sun_color;
@@ -40,9 +35,9 @@ void main() {
     float spotlight_lum = float(spotlight_cos >= spotlight_first_cos);
     vec3 spotlight = (spotlight_coef * spotlight_lum) * spotlight_color;
 
-    // calculate unconditional light
-    vec3 uncond_light = uncond_light_coef * sun_color;
+    // calculate ambient light
+    vec3 ambient_light = ambient_light_coef * sun_color;
 
     // calculate the result
-    out_col = texture(tex_2d, vert_tex) * vec4(sun_light + spotlight + uncond_light, 1.0);
+    out_col = texture(tex_2d, vert_tex) * vec4(sun_light + spotlight + ambient_light, 1.0);
 }
